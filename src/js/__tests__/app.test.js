c476eaf61fef8f3507f8b39af5a6df7a84148837
mosius27 <mosius27@yandex.ru>
2023-12-02 11:07:40 +0300

    .

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,62 @@
+import orderByProps from '../app';
+
+test('Сортировка вначале по массиву, потом по алфавиту name level', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const order = orderByProps(obj, ['name', 'level']);
+  const result = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ];
+  expect(order).toEqual(result);
+});
+
+test('Сортировка вначале по массиву, потом по алфавиту health level', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const order = orderByProps(obj, ['health', 'level']);
+  const result = [
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'name', value: 'мечник' },
+  ];
+  expect(order).toEqual(result);
+});
+
+
+test('Сортировка вначале по массиву, потом по алфавиту defence', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const order = orderByProps(obj, ['defence']);
+  const result = [
+    { key: 'defence', value: 40 },
+    { key: 'attack', value: 80 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
+  ];
+  expect(order).toEqual(result);
+});
+
+test('Сортировка по алфавиту', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const order = orderByProps(obj, []);
+  const result = [
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
+  ];
+  expect(order).toEqual(result);
+});
---
+++ src/js/app.js
@@ -0,0 +1,37 @@
+const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  
+  export default function orderByProps(object, array) {
+    const result = [];
+  
+    for (const prop in object) {
+      if (Object.prototype.hasOwnProperty.call(object, prop) && !array.includes(prop)) {
+        result.push({ key: prop, value: object[prop] });
+      }
+    }
+  
+    result.sort((a, b) => {
+      if (a.key > b.key) {
+        return 1;
+      }
+      return -1;
+    });
+  
+    for (let i = array.length - 1; i >= 0; i -= 1) {
+      if (Object.prototype.hasOwnProperty.call(object, array[i])) {
+        result.unshift({
+          key: array[i],
+          value: object[array[i]],
+        });
+      }
+    }
+  
+    return result;
+  }
+  
+  
+  console.log(orderByProps(obj, ['name', 'level']));
+  console.log(obj);
+  console.log(orderByProps(obj, ['health', 'level']));
+  console.log(obj);
